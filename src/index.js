e615fc1a729c26b388a523e0a21009f3203f55d5
Koboh Kosaka <kobohkosaka@outlook.com>
2021-03-29 13:56:07 -0700

    Separate login component to file

--- src/index.js
+++ src/index.js
@@ -1,70 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { w3cwebsocket } from 'websocket';
+import React from "react";
+import ReactDOM from "react-dom";
+import { w3cwebsocket } from "websocket";
+import Chat from "./chat";
+import Login from "./login";
+import "./index.css";
 
-const client = new w3cwebsocket('ws://localhost:8000');
+const client = new w3cwebsocket("ws://localhost:8000");
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       isLoggedIn: false,
-      user: '',
+      user: "",
+      message: "",
       messages: [],
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-  componentDidMount() {
-    client.onopen = () => {
-      console.log("websocket client connected")
-    }
-    client.onmessage= (message) => {
-      const data = JSON.parse(message.data);
-      console.log(data);
-      if(data.type === "message") {
-        this.setState((state) => ({
-          messages: [...state.messages,
-          {
-            msg: data.msg,
-            user: data.user,
-          }]
-        })
-        );
-      }
-    }
-  }
-  handleChange(e) {
-    this.setState({
-      [e.target.id]: e.target.value,
-    })
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleSend(value) {
-    client.send(JSON.stringify({
-      type: "message",
-      msg: value,
-      user: this.state.user,
-    }));
+  componentDidMount() {
+    client.onopen = () => {
+      console.log("websocket client connected");
+    };
+    client.onmessage = (message) => {
+      const data = JSON.parse(message.data);
+      console.log(data);
+      if (data.type === "message") {
+        this.setState((state) => ({
+          messages: [
+            ...state.messages,
+            {
+              msg: data.msg,
+              user: data.user,
+            },
+          ],
+        }));
+      }
+    };
+  }
+  handleChange(e) {
+    var value = e.target.id === "isLoggedIn" ? true : e.target.value;
+    this.setState({
+      [e.target.id]: value,
+    });
+  }
+
+  handleSend() {
+    client.send(
+      JSON.stringify({
+        type: "message",
+        msg: this.state.message,
+        user: this.state.user,
+      })
+    );
+    this.setState({
+      message: "",
+    });
   }
 
   render() {
     return (
       <div className="main">
-        {this.state.isLoggedIn ?
-        <div>
-          <button onClick={() => this.handleSend("joe")} >Send</button>
-          {this.state.messages.map((message) => (
-            <p>{message.msg} {message.user}</p>))}
-        </div>
-        :
-        <div>
-          <input type="text" id="user" placeholder='username' value={this.state.user} onChange={this.handleChange}></input>
-          <button onClick={() => this.setState({isLoggedIn: true})}>login</button>
-        </div>
-        }
+        {this.state.isLoggedIn ? (
+          <div className="chats">
+            <div className="message-bar">
+              <input
+                type="text"
+                id="message"
+                placeholder="message"
+                value={this.state.message}
+                onChange={this.handleChange}
+              ></input>
+              <button onClick={() => this.handleSend()}>send</button>
+            </div>
+            {this.state.messages.map((message, i) => (
+              <Chat key={i} message={message} userName={this.state.user} />
+            ))}
+          </div>
+        ) : (
+          <Login handleChange={this.handleChange} user={this.state.user} />
+        )}
       </div>
-    )
+    );
   }
-};
+}
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
