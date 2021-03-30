067a711d5154873f8c9221eeae01831cc8df1acc
Koboh Kosaka <kobohkosaka@outlook.com>
2021-03-30 16:22:20 -0700

    Send message to clients upon new client/and close client

--- server/index.js
+++ server/index.js
@@ -11,7 +11,53 @@ const wsServer = new webSocketServer({
 });
 
 const clients = {};
+// var onlineUsers = Object.keys(clients).length;
 
+wsServer.on("request", function (request) {
+  var userID = getUniqueID();
+  const connection = request.accept(null, request.origin);
+  console.log("request origin", request.origin);
+  clients[userID] = connection;
+
+  //on message handler, looping clients and forwarding message to them
+  //if the utfdata is of type login, store the username into the userID object, then send all clients a message that a new user has entered
+  connection.on("message", function (message) {
+    if (message.type === "utf8") {
+      var data = JSON.parse(message.utf8Data);
+      if (data.type === "login") {
+        var user = data.user;
+        clients[userID].username = user;
+        newUserEntrance(user);
+      } else {
+        for (key in clients) {
+          data.userCount = Object.keys(clients).length;
+          clients[key].sendUTF(JSON.stringify(data));
+        }
+      }
+    }
+  });
+
+  //when connection closes, send all clients a message that this client is leaving,
+  //then delete client from clients object
+  connection.on("close", function (message) {
+    var username = clients[userID].username;
+    delete clients[userID];
+
+    for (key in clients) {
+      clients[key].sendUTF(
+        JSON.stringify({
+          type: "message",
+          msg: `has left the chat.`,
+          user: username,
+          color: "grey",
+          userCount: Object.keys(clients).length,
+        })
+      );
+    }
+  });
+});
+
+//helper functions
 const getUniqueID = () => {
   const s4 = () =>
     Math.floor((1 + Math.random()) * 0x10000)
@@ -28,61 +74,8 @@ const newUserEntrance = (username) => {
         msg: `has entered the chat.`,
         user: username,
         color: "grey",
+        userCount: Object.keys(clients).length,
       })
     );
   }
 };
-
-wsServer.on("request", function (request) {
-  var userID = getUniqueID();
-  const connection = request.accept(null, request.origin);
-  // console.log("connection!", connection);
-  //store connection with generated userID
-  clients[userID] = connection;
-  console.log(
-    "connected: " + userID + " in " + Object.getOwnPropertyNames(clients)
-  );
-  connection.on("login", (message) => {
-    console.log("logging IN!");
-  });
-  // newUserEntrance(*)
-
-  var onlineUsers = Object.keys(clients).length;
-  var userCount = JSON.stringify({
-    type: "userCount",
-    userCount: onlineUsers,
-  });
-  //on message handler, looping clients and forwarding message to them
-  connection.on("message", function (message) {
-    if (message.type === "utf8") {
-      console.log("Received message: ", message.utf8Data);
-      console.log("Received message: ", message);
-      var jsonData = JSON.parse(message.utf8Data);
-      if (jsonData.type === "login") {
-        var user = jsonData.user;
-        clients[userID].username = user;
-        newUserEntrance(user);
-      } else {
-        for (key in clients) {
-          clients[key].sendUTF(message.utf8Data);
-          clients[key].sendUTF(userCount);
-          // console.log('sent message to: ', clients[key]);
-        }
-      }
-    }
-  });
-
-  connection.on("close", function (message) {
-    for (key in clients) {
-      clients[key].sendUTF(
-        JSON.stringify({
-          type: "message",
-          msg: `has left the chat.`,
-          user: clients[userID].username,
-          color: "grey",
-        })
-      );
-    }
-    delete clients[userID];
-  });
-});
